adbc6e6fdd28a1e01e3c0e41e2d7ccca00618307
aledeejk <alexadeevajk2006@yandex.ru>
2025-12-21 21:35:17 +0300

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_21_142228) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_21_180508) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -22,9 +22,19 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_21_142228) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
+    t.text "content"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "comments", "posts"
